0af2b66b657031a4a26bd682dbe6ca20aa4c9fb6
Ali Rassolie <ali.rassolie@gmail.com>
2017-05-02 18:54:11 +0200

    Add files via upload
    Optimizations

--- annuc.py
+++ annuc.py
@@ -2,7 +2,7 @@
 # (c) 2017 Ali Rassolie
 # Lumina sequencing
 
-import time	
+
 class annuc:
 	def __init__(self, **kwargs):
 		# Imports
@@ -13,8 +13,10 @@ class annuc:
 		self.threads = kwargs["threads"]
 		self.count = kwargs["count"]
 		self.output = kwargs["output"]
+		
 		# where to start from. 
 		self.pos_in_iter = 147
+		self.endstart    = list()
 
 	def specific_input(self):
 		with open(self.spec_input, "r") as spec_file:	
@@ -36,43 +38,122 @@ class annuc:
 						pass	
 				else:
 					pass
+
+	def chunk_search(self):
+		import re, itertools
+
+		with open(self.hass_input, "r") as file:
+			while True:
+				search_list = list(itertools.islice(file, 1000))
+				if not search_list:
+					break
+
+
+				search_for = re.compile("{}\\t{}\\t.*".format(self.chr_, self.pos))
+				finding = list(filter(search_for.match, search_list))
+				if finding:
+					finding_list = finding[0].split("\t")
+					result = [finding_list[:3], finding_list[4:5]]
+					yield result
+				
+				elif not finding:
+					pass
+				
+				else:
+					print("We have an issue")
 					
 
-	def filter(self):
-		with open(self.output, "a") as file:
+	def filter_hassle(self):
+		import time	
+		with open(self.output, "w") as file:
 			spec_gen = self.specific_input()
 			hass_gen = self.hass()
+			chunk_gen = self.chunk_search()
 			try:
 				text = ""
 				count = 0
 				start = time.time()
+				start2 = time.time()
 				for self.chr_, self.pos in spec_gen:
 					append_this = next(self.hass())
+					# append_this = next(self.chunk_search())
 					text = text + "{}\t{}\t{}\t{}\n".format(*append_this)
-					if count == self.count:
-						file.write(text)
-						text = ""
-						end = time.time()
-						print(end-start)
-						start = time.time()
+					if count == 100:
+						end2 = time.time()
+						print(end2-start2)
+						start2 = time.time()
 						count = 0
 					count += 1
-			
-			except Exception as e:
-				print("Ehh{}".format(e))
+				
+				file.write(text)
+				end = time.time()
+				self.endstart = end - start
+				
+			except:
+				raise Exception
 
+	def filter_chunk(self):
+		import time	
+		with open(self.output, "w") as file:
+			spec_gen = self.specific_input()
+			hass_gen = self.hass()
+			chunk_gen = self.chunk_search()
+			try:
+				text = ""
+				count = 0
+				start = time.time()
+				start2 = time.time()
+				for self.chr_, self.pos in spec_gen:
+					append_this = next(self.hass())
+					# append_this = next(self.chunk_search())
+					text = text + "{}\t{}\t{}\t{}\n".format(*append_this)
+					if count == 100:
+						end2 = time.time()
+						print(end2-start2)
+						start2 = time.time()
+						count = 0
+					count += 1
+				
+				file.write(text)
+				end = time.time()
+				self.endstart = end - start
+				
+			except:
+				raise Exception
+
+
+
+
+	def plot_results(self, infile=None):
+		import matplotlib.pyplot as plt
+	
+
+
+	def clean(self):
+		with open("malbac_4.freebayes.bwa.vcf", "rb") as file_in:
+			with open("cleaned_hassle.vcf", "wb") as file_out:
+				for i, line in enumerate(file_in):
+					if i < 50000:
+						file_out.write(line)
+					else:
+						break
+		with open("malbac_4_vcfoutput", "rb") as file_in:
+			with open("cleaned_sample", "wb") as file_out:
+				for i, line in enumerate(file_in):
+					if i < 10000:
+						file_out.write(line)
+					else:
+						break
+
+	def timed_completion(self):
+		print(self.endstart)
 
 
 
 
 if __name__ == '__main__':
-	a = annuc(dictinput="malbac_4_vcfoutput", hass="malbac_4.freebayes.bwa.vcf", threads=2, count=100, output="1.vcf")
-	a.filter()
+	
 
-
-
-
-	# with open("malbac_4.freebayes.bwa.vcf", "r") as hass_file:
-	# 	print(hass_file.tell())
-		# a = hass_file.readlines(100000)
-		# print(a)
+	a = annuc(dictinput="cleaned_sample", hass="cleaned_hassle.vcf", threads=2, count=100, output="3.vcf")
+	a.filter_hassle()
+	a.timed_completion()
